35598c1f8d71d64acc0df36a4076b945e4f3988b
frisyk <frisnadi1@gmail.com>
2025-05-05 07:15:45 +0700

    refactor: password validation

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -13,9 +13,9 @@ export const SignupFormSchema = z.object({
     .min(8, { message: 'Be at least 8 characters long' })
     .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
     .regex(/[0-9]/, { message: 'Contain at least one number.' })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: 'Contain at least one special character.',
-    })
+    // .regex(/[^a-zA-Z0-9]/, {
+    //   message: 'Contain at least one special character.',
+    // })
     .trim(),
 });
 
