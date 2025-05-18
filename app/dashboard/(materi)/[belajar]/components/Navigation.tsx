6509ea4d0e4c621afed2e69285a52b3cf0907ec2
frisyk <frisnadi1@gmail.com>
2025-05-18 14:47:22 +0700

    style: optimize navigation

--- app/dashboard/(materi)/[belajar]/components/Navigation.tsx
+++ app/dashboard/(materi)/[belajar]/components/Navigation.tsx
@@ -5,7 +5,7 @@ import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 
 export const NavigationComponent: React.FC<NavigationComponentProps> = ({ currentQuestion, totalQuestions, onPrevious, onNext, onSubmit, onSelect }) => {
     return (
-        <div className="flex justify-between items-center m-3">
+        <div className="flex justify-between items-center overflow-hidden m-3">
             <button 
                 onClick={onPrevious} 
                 className={`flex items-center px-2 py-2 rounded-lg ${currentQuestion === 1 ? 'bg-gray-600 text-gray-400 cursor-not-allowed' : 'dark:bg-gray-800 outline-1 outline dark:outline-none cursor-pointer'}`}
@@ -13,12 +13,12 @@ export const NavigationComponent: React.FC<NavigationComponentProps> = ({ curren
             >
                 <MdNavigateBefore className="w-6 h-6 mx-2" /><span className="hidden md:block">Sebelumnya</span>
             </button>
-            <div className="flex space-x-2 ">
+            <div className="flex space-x-2 overflow-x-scroll">
                 {[...Array(totalQuestions)].map((_, i) => (
                     <div 
                         key={i} 
                         onClick={() => onSelect(i + 1)}
-                        className={`w-8 h-8 flex cursor-pointer items-center justify-center rounded-lg ${i + 1 === currentQuestion ? 'bg-teal-400 text-gray-900' : 'dark:bg-gray-800 outline-1 outline dark:outline-none'}`}
+                        className={`w-10 h-10 flex cursor-pointer items-center justify-center rounded-lg ${i + 1 === currentQuestion ? 'bg-teal-400 text-gray-900' : 'dark:bg-gray-800 outline-1 outline dark:outline-none'}`}
                     >
                         {i + 1}
                     </div>
